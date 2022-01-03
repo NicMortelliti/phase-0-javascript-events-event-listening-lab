f91286f8bbc64d7cc1ebdb02147f416d4529dc3f
Nicolas <npmortelliti@gmail.com>
2022-01-02 21:05:08 -0800

    Completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    input.addEventListener('click', clickAlert)
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+const input = document.getElementById('input');
+
+addingEventListener();
